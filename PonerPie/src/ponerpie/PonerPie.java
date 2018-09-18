c207903cf755251c0c159534fd1ad50920c28553
gustavofq <quintana.gustavo.f@gmail.com>
2018-09-18 20:47:49 -0300

    quita el "_" del nombre del documento pdf

--- PonerPie/src/ponerpie/PonerPie.java
+++ PonerPie/src/ponerpie/PonerPie.java
@@ -1,6 +1,5 @@
 
 package ponerpie;
-
 import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
@@ -8,22 +7,28 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
-
+/*
+autor : Quintana Gustavo Fernando.
+parametros: 
+-args[0] :Direccion del directorio contenedor de los documentos 
+pdf sin poner pie.
+-args[1]: direccion del directorio contenedor de los documentos pdf con
+el pie puesto.
+*/
 public class PonerPie {
     public static void main(String[] args) throws IOException {
-        
         String text = "";
         File _dir = new File(args[0]);
         File[] ficheros = _dir.listFiles();
-        System.out.println(ficheros.length);
+        String path = new java.io.File(".").getCanonicalPath();
+        System.out.println(path);
         if(_dir != null){
             for (int x=0;x<ficheros.length;x++){
                 File file = new File(args[0]+"\\"+ficheros[x].getName());
-                System.out.println("archivo a modificar: " + args[0] + "\\" + ficheros[x].getName());
+                System.out.println("Agregando pie a: " + args[0] + "\\" + ficheros[x].getName());
                 PDDocument document = PDDocument.load(file);
                 int noOfPages= document.getNumberOfPages();
                 for(int i= 0;i < noOfPages; i++){
-                    System.out.println("pagina: " + i + " numero de pagina: " + noOfPages);
                     PDPage page = document.getPage(i);
                     PDPageContentStream contentStream = new PDPageContentStream(document, page,PDPageContentStream.AppendMode.APPEND, true, true); 
                     contentStream.beginText(); 
@@ -38,9 +43,11 @@ public class PonerPie {
                     contentStream.close();
                 }
             System.out.println("archivo guardado: "+args[1] +"\\" + "_" +  ficheros[x].getName());
-            document.save(args[1] +"\\" + "_" +  ficheros[x].getName());
+            String nombreNuevo = ficheros[x].getName().substring(0, ficheros[x].getName().length());
+            document.save(args[1] +"\\" + "" + nombreNuevo);
             document.close();
             }
         }
-    } 
+    System.out.println("El proceso ha terminado con exito.");    
+    }   
 }
