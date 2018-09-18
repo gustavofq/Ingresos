43cdd45937369682d78b9beed1363083eb21afa3
gustavofq <quintana.gustavo.f@gmail.com>
2018-09-18 19:42:40 -0300

    recorre la lista de documentos

--- PonerPie/src/ponerpie/PonerPie.java
+++ PonerPie/src/ponerpie/PonerPie.java
@@ -1,43 +1,46 @@
 
 package ponerpie;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 public class PonerPie {
     public static void main(String[] args) throws IOException {
-        File file = new File("C:\\Users\\gustavo\\Desktop\\Propuesta de proyecto(1).pdf");
-        PDDocument document = PDDocument.load(file);
-        int noOfPages= document.getNumberOfPages();
-        String text = "";
-        for(int i= 0;i < noOfPages; i++){
-            System.out.println("pagina: " + i + " numero de pagina: " + noOfPages);
-            PDPage page = document.getPage(i);
-            PDPageContentStream contentStream = new PDPageContentStream(document, page,PDPageContentStream.AppendMode.APPEND, true, true); 
-            contentStream.beginText(); 
-            contentStream.newLineAtOffset(200, 50);
-            PDType0Font font = PDType0Font.load(document, new File("C:\\Users\\gustavo\\Documents\\NetBeansProjects\\PonerPie\\src\\Swis721_BlkOul_BT_Black.ttf"));
-            
-            
-            
-            contentStream.setFont(font, 20 );
-            
-            
-            contentStream.setLeading(14.5f);
-            text="C.D.D.I - Página " + (i + 1) + " de " + noOfPages ;
-            contentStream.showText(text);
-            contentStream.endText();
-            contentStream.close();
-        }
-        document.save("C:\\Users\\gustavo\\Desktop\\_Propuesta de proyecto(1).pdf");
-        document.close();
         
-    }
-    
+        String text = "";
+        File _dir = new File(args[0]);
+        File[] ficheros = _dir.listFiles();
+        System.out.println(ficheros.length);
+        if(_dir != null){
+            for (int x=0;x<ficheros.length;x++){
+                File file = new File(args[0]+"\\"+ficheros[x].getName());
+                System.out.println("archivo a modificar: " + args[0] + "\\" + ficheros[x].getName());
+                PDDocument document = PDDocument.load(file);
+                int noOfPages= document.getNumberOfPages();
+                for(int i= 0;i < noOfPages; i++){
+                    System.out.println("pagina: " + i + " numero de pagina: " + noOfPages);
+                    PDPage page = document.getPage(i);
+                    PDPageContentStream contentStream = new PDPageContentStream(document, page,PDPageContentStream.AppendMode.APPEND, true, true); 
+                    contentStream.beginText(); 
+                    contentStream.newLineAtOffset(200, 50);
+                    contentStream.setNonStrokingColor(Color.BLUE);
+                    PDType0Font font = PDType0Font.load(document, new File("C:\\Users\\gustavo\\Documents\\NetBeansProjects\\PonerPie\\src\\Swis721_BlkOul_BT_Black.ttf"));
+                    contentStream.setFont(font, 20 );
+                    contentStream.setLeading(14.5f);
+                    text="C.D.D.I - Página " + (i + 1) + " de " + noOfPages ;
+                    contentStream.showText(text);
+                    contentStream.endText();
+                    contentStream.close();
+                }
+            System.out.println("archivo guardado: "+args[1] +"\\" + "_" +  ficheros[x].getName());
+            document.save(args[1] +"\\" + "_" +  ficheros[x].getName());
+            document.close();
+            }
+        }
+    } 
 }
