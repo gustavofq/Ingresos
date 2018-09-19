1275fe2d371ebcdad9192104ed1e90bcdb71a3a9
gustavofq <quintana.gustavo.f@gmail.com>
2018-09-18 21:00:11 -0300

    se agrego tipo de fuente

--- PonerPie/src/ponerpie/PonerPie.java
+++ PonerPie/src/ponerpie/PonerPie.java
@@ -3,26 +3,32 @@ package ponerpie;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 public class PonerPie {
     public static void main(String[] args) throws IOException {
         File file = new File("C:\\Users\\gustavo\\Desktop\\Propuesta de proyecto(1).pdf");
         PDDocument document = PDDocument.load(file);
-        if(document)
         int noOfPages= document.getNumberOfPages();
         String text = "";
         for(int i= 0;i < noOfPages; i++){
             System.out.println("pagina: " + i + " numero de pagina: " + noOfPages);
             PDPage page = document.getPage(i);
-            PDPageContentStream contentStream = new PDPageContentStream(document, page);
-            
+            PDPageContentStream contentStream = new PDPageContentStream(document, page,PDPageContentStream.AppendMode.APPEND, true, true); 
             contentStream.beginText(); 
-            contentStream.newLineAtOffset(25, 700);
-            contentStream.setFont(PDType1Font.TIMES_ROMAN, 16 );
+            contentStream.newLineAtOffset(200, 50);
+            PDType0Font font = PDType0Font.load(document, new File("C:\\Users\\gustavo\\Documents\\NetBeansProjects\\PonerPie\\src\\Swis721_BlkOul_BT_Black.ttf"));
+            
+            
+            
+            contentStream.setFont(font, 20 );
+            
+            
             contentStream.setLeading(14.5f);
             text="C.D.D.I - Página " + (i + 1) + " de " + noOfPages ;
             contentStream.showText(text);
